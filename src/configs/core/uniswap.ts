1134bc10862c49d19da5783c8d9eca59a4384539
Ye Yu <nearjilt@gmail.com>
2025-10-03 09:33:23 +0800

    chore(contract): add contract addresses and ABIs

---
+++ src/configs/core/uniswap.ts
@@ -0,0 +1,7 @@
+import type { Address } from 'viem';
+
+export const uniswapFactoryContractAddress =
+  '0xF62c03E08ada871A0bEb309762E260a7a6a880E6' as Address;
+
+export const uniswapV2Router02ContractAddress =
+  '0xeE567Fe1712Faf6149d80dA1E6934E354124CfE3' as Address;
