80e684d62f7fa25e1e532ab6fdf6aebc38e12550
Ye Yu <nearjilt@gmail.com>
2025-10-09 11:54:28 +0800

    feat: add contract addresses and abis

--- src/configs/core/uniswap.ts
+++ src/configs/core/uniswap.ts
@@ -5,3 +5,9 @@ export const uniswapFactoryContractAddress =
 
 export const uniswapV2Router02ContractAddress =
   '0xeE567Fe1712Faf6149d80dA1E6934E354124CfE3' as Address;
+
+export const uniswapV3FactoryContractAddress =
+  '0x0227628f3F023bb0B980b67D528571c95c6DaC1c' as Address;
+
+export const uniswapV3Router02ContractAddress =
+  '0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E' as Address;
